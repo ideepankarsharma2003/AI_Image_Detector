3f943ee01b2d57eb4ea5243b7202812113c51690
Deepankar Sharma <deepankarsharma2003@gmail.com>
2024-02-06 13:09:57 +0000

    added AI detector

---
+++ scripts/upload_dataset.py
@@ -0,0 +1,21 @@
+from datasets import load_dataset
+
+print(f"{'>'*20}[LOADING DATASET]{'<'*20}")
+dataset = load_dataset("imagefolder", data_dir="images")
+print(f"{'>'*20}[LOADED DATASET]{'<'*20}")
+
+print(dataset)
+dataset= dataset.shuffle(seed=45)
+
+# dataset.save_to_disk("MidjourneV6_Image_small")
+
+
+def push_dataset(dataset):
+    try:
+        dataset.push_to_hub("ideepankarsharma2003/ImageClassificationStableDiffusion_small")
+    except:
+        push_dataset(dataset)
+
+push_dataset(dataset)
+
+print(f"{'>'*20}[ALL DONE]{'<'*20}")
